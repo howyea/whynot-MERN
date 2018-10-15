ced14ea7001eb02ea753f64601f19e831f410502
huiyuye <yhynew@126.com>
2018-10-15 16:21:34 +0800

    perfect gulp watch to working when error

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var util=require('gulp-util');
+var plumber=require('gulp-plumber');
 var tsProject = ts.createProject("./server/tsconfig.json");
-
+function errorHandle(e){
+	util.beep(); //控制台发声,错误时beep一下
+	util.log(e);
+}
 gulp.task("default", function () {
     return gulp.src('server/**/*.ts')
+        .pipe( plumber({errorHandler:errorHandle}) )  //在处理前注册plumber
         .pipe(tsProject())
         .js.pipe(gulp.dest("server_d"));
 });
