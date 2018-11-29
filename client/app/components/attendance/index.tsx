97ec86525e87c590a9281d870d04ce48e80985e1
huiyuye <yhynew@126.com>
2018-11-29 17:13:05 +0800

    test the threejs

--- client/app/components/attendance/index.tsx
+++ client/app/components/attendance/index.tsx
@@ -1,20 +1,19 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { Toast } from "antd-mobile";
+import * as THREE from 'three';
 import { AttendanceStyled } from "./styled";
 import { MyIcons } from "../styled";
 import { login, signIn } from "./interface";
 export interface AttendanceProps {
     
 }
- 
-export interface AttendanceState {
-    
-}
- 
-class Attendance extends React.Component<AttendanceProps, AttendanceState> {
-    state = { 
 
+const initialState = {
+}
+type State = Readonly<typeof initialState>;
+class Attendance extends React.Component<AttendanceProps> {
+    readonly state: State = {
     }
     signInFunc = async () => {
         const _res = await signIn(this.signInParams);
@@ -45,14 +44,37 @@ class Attendance extends React.Component<AttendanceProps, AttendanceState> {
             Toast.info(_res.desc);
         }
     }
+    renderCanvas = () => {
+        var scene = new THREE.Scene();
+        
+        var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+        
+        var renderer = new THREE.WebGLRenderer();
+        
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setClearColor('#000',1.0);
+        document.getElementById('box').appendChild(renderer.domElement);
+        var geometry = new THREE.CubeGeometry(1,1,1);
+        var material = new THREE.MeshBasicMaterial({color: "#ffffff"});
+        var cube = new THREE.Mesh(geometry, material); scene.add(cube);
+        camera.position.z = 5;
+        function render() {
+            requestAnimationFrame(render);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        }
+        render();
+    }
     componentDidMount() {
         this.loginFunc();
+        this.renderCanvas();
     }
     render() { 
         return (  
             <AttendanceStyled>
-                <div className="box">
-                    <MyIcons onClick={ this.signInFunc } width="1.92" url={require('../../images/baseline_fingerprint_white_48.png')}/>
+                <div id="box" className="box">
+                    <MyIcons className="icon" onClick={ this.signInFunc } width="1.92" url={require('../../images/baseline_fingerprint_white_48.png')}/>
                 </div>
             </AttendanceStyled>
         );
--- client/app/components/attendance/styled.ts
+++ client/app/components/attendance/styled.ts
@@ -9,10 +9,17 @@ export const AttendanceStyled = styled.div`
     height: ${ window.screen.height - 113}px;
     ${ columnVerticalCenter }
     .box {
-        width: 1.92rem;
-        height: 1.92rem;
+        width: 100%;
+        height: ${ window.screen.height - 100}px;
         border-radius: 50%;
         background-color: #3c4f9b;
-        ${ columnVerticalCenter }
+        .icon {
+            position: absolute;
+            z-index: 2;
+        }
+        canvas {
+            position: absolute;
+            z-index: 1;
+        }
     }
 `;
