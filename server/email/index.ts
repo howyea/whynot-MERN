525e6589d5cec70145b66b8be203c430d38be57f
huiyuye <yhynew@126.com>
2019-03-22 16:17:23 +0800

    调试邮件服务

--- server/email/index.ts
+++ server/email/index.ts
@@ -35,8 +35,8 @@ let EmailTo = "1070231248@qq.com";
 let EmailSubject = "一封暖暖的小邮件";
 
 //每日发送时间
-let EmailHour = 16;
-let EmialMinminute= 10;
+let EmailHour = 10;
+let EmialMinminute= 20;
 
 // 爬取数据的url
 const OneUrl = "http://wufazhuce.com/";
